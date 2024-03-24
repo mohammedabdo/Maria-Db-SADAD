d048da22ec071ad5713b55a0568b8c62b6e5fad9
mohammedabdo <m.algadir@gmail.com>
2024-03-24 04:59:58 +0200

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -293,8 +293,8 @@ MariaLft = await getBuffer(ppuser)
 		if (!global.welcome) return;	
                 const Mariabuffer = await getBuffer(ppuser)
                 let MariaName = num
-                const xtime = moment.tz('Asia/Kolkata').format('HH:mm:ss')
-	            const xdate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
+                const xtime = moment.tz('Africa/Cairo').format('HH:mm:ss')
+	            const xdate = moment.tz('Africa/Cairo').format('DD/MM/YYYY')
 	            const xmembers = metadata.participants.length
                 Mariabody = `┌──⊰ 🎗𝑾𝑬𝑳𝑳𝑪𝑶𝑴𝑬🎗⊰
 │⊳  🌐 To: ${metadata.subject}
@@ -317,8 +317,8 @@ Maria.sendMessage(anu.id,
 "sourceUrl": `${link}`}}})
                 } else if (anu.action == 'remove') {
                 	const Mariabuffer = await getBuffer(ppuser)
-                    const Mariatime = moment.tz('Asia/Kolkata').format('HH:mm:ss')
-	                const Mariadate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
+                    const Mariatime = moment.tz('Africa/Cairo').format('HH:mm:ss')
+	                const Mariadate = moment.tz('Africa/Cairo').format('DD/MM/YYYY')
                 	let MariaName = num
                     const Mariamembers = metadata.participants.length
   Mariabody = `┌──⊰🍁𝑭𝑨𝑹𝑬𝑾𝑬𝑳𝑳🍁⊰
@@ -345,8 +345,8 @@ Maria.sendMessage(anu.id,
 "sourceUrl": `${link}`}}})
 } else if (anu.action == 'promote') {
 const Mariabuffer = await getBuffer(ppuser)
-const Mariatime = moment.tz('Asia/Kolkata').format('HH:mm:ss')
-const Mariadate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
+const Mariatime = moment.tz('Africa/Cairo').format('HH:mm:ss')
+const Mariadate = moment.tz('Africa/Cairo').format('DD/MM/YYYY')
 let MariaName = num
 Mariabody = ` 𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘀🎉 @${MariaName.split("@")[0]}, you have been *promoted* to *admin* 🥳`
    Maria.sendMessage(anu.id,
@@ -363,8 +363,8 @@ Mariabody = ` 𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘀🎉 @${MariaName.split("@")[0]}, 
 "sourceUrl": `${link}`}}})
 } else if (anu.action == 'demote') {
 const Mariabuffer = await getBuffer(ppuser)
-const Mariatime = moment.tz('Asia/Kolkata').format('HH:mm:ss')
-const Mariadate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
+const Mariatime = moment.tz('Africa/Cairo').format('HH:mm:ss')
+const Mariadate = moment.tz('Africa/Cairo').format('DD/MM/YYYY')
 let MariaName = num
 Mariabody = `𝗢𝗼𝗽𝘀‼️ @${MariaName.split("@")[0]}, you have been *demoted* from *admin* 😬`
 Maria.sendMessage(anu.id,
